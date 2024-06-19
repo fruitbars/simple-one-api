29cff6ef6088fac08582eba46e61503e68933bdf
fruitbars <huiyanghu@gmail.com>
2024-06-19 18:46:01 +0800

    oai消息常见函数增加

--- pkg/common/oai_message_utils.go
+++ pkg/common/oai_message_utils.go
@@ -1,1 +1,27 @@
 package common
+
+import (
+	"github.com/sashabaranov/go-openai"
+	"strings"
+)
+
+// ProcessMessages 根据消息的角色处理聊天历史。
+func ConvertSystemMessages2NoSystem(oaiReq []openai.ChatCompletionMessage) []openai.ChatCompletionMessage {
+	var systemQuery string
+	if len(oaiReq) == 0 {
+		return oaiReq
+	}
+
+	// 如果第一条消息的角色是 "system"，根据条件处理消息
+	if strings.ToLower(oaiReq[0].Role) == "system" {
+		if len(oaiReq) == 1 {
+			oaiReq[0].Role = "user"
+		} else {
+			systemQuery = oaiReq[0].Content
+			oaiReq = oaiReq[1:] // 移除系统消息
+			oaiReq[0].Content = systemQuery + "\n" + oaiReq[0].Content
+		}
+	}
+
+	return oaiReq
+}
