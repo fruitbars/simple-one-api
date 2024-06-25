f663223e82cc33231b922946b2da2f8d36d1f32c
fruitbars <huiyanghu@gmail.com>
2024-06-25 23:55:58 +0800

    包名字修改

--- pkg/openai/openai_request.go
+++ pkg/openai/openai_request.go
@@ -2,30 +2,30 @@ package myopenai
 
 import (
 	"encoding/json"
-	"simple-one-api/pkg/common"
+	"simple-one-api/pkg/mycommon"
 )
 
 // RequestBody 定义 API 请求的主体结构
 type OpenAIRequest struct {
-	Model            string           `json:"model"`
-	Messages         []common.Message `json:"messages"`
-	FrequencyPenalty *float32         `json:"frequency_penalty,omitempty"`
-	LogitBias        map[int]int      `json:"logit_bias,omitempty"`
-	LogProbs         *bool            `json:"logprobs,omitempty"`
-	TopLogProbs      *int             `json:"top_logprobs,omitempty"`
-	MaxTokens        *int             `json:"max_tokens,omitempty"`
-	N                *int             `json:"n,omitempty"`
-	PresencePenalty  *float32         `json:"presence_penalty,omitempty"`
-	ResponseFormat   *ResponseFormat  `json:"response_format,omitempty"`
-	Seed             *int             `json:"seed,omitempty"`
-	Stop             []string         `json:"stop,omitempty"`
-	Stream           *bool            `json:"stream,omitempty"`
-	StreamOptions    *StreamOptions   `json:"stream_options,omitempty"`
-	Temperature      *float32         `json:"temperature,omitempty"`
-	TopP             *float32         `json:"top_p,omitempty"`
-	Tools            []Tool           `json:"tools,omitempty"`
-	ToolChoice       json.RawMessage  `json:"tool_choice,omitempty"`
-	User             *string          `json:"user,omitempty"`
+	Model            string             `json:"model"`
+	Messages         []mycommon.Message `json:"messages"`
+	FrequencyPenalty *float32           `json:"frequency_penalty,omitempty"`
+	LogitBias        map[int]int        `json:"logit_bias,omitempty"`
+	LogProbs         *bool              `json:"logprobs,omitempty"`
+	TopLogProbs      *int               `json:"top_logprobs,omitempty"`
+	MaxTokens        *int               `json:"max_tokens,omitempty"`
+	N                *int               `json:"n,omitempty"`
+	PresencePenalty  *float32           `json:"presence_penalty,omitempty"`
+	ResponseFormat   *ResponseFormat    `json:"response_format,omitempty"`
+	Seed             *int               `json:"seed,omitempty"`
+	Stop             []string           `json:"stop,omitempty"`
+	Stream           *bool              `json:"stream,omitempty"`
+	StreamOptions    *StreamOptions     `json:"stream_options,omitempty"`
+	Temperature      *float32           `json:"temperature,omitempty"`
+	TopP             *float32           `json:"top_p,omitempty"`
+	Tools            []Tool             `json:"tools,omitempty"`
+	ToolChoice       json.RawMessage    `json:"tool_choice,omitempty"`
+	User             *string            `json:"user,omitempty"`
 }
 
 // ResponseFormat 定义响应格式的结构
