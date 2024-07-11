4b10bbb11854839de75d8e499e7821fc53e70dec
fruitbars <huiyanghu@gmail.com>
2024-07-11 13:56:33 +0800

    增加GCP的一些字段

--- pkg/config/config_keyname.go
+++ pkg/config/config_keyname.go
@@ -10,5 +10,10 @@ const KEYNAME_API_SECRET = "api_secret"
 const KEYNAME_DOMAIN = "domain"
 const KEYNAME_ACCESS_KEY = "access_key"
 
+const KEYNAME_GCP_PROJECT_ID = "project_id"
+const KEYNAME_GCP_LOCATION = "location"
+const KEYNAME_GCP_MODEL_ID = "model_id"
+const KEYNAME_GCP_JSON_FILE = "json_file"
+
 const KEYNAME_RANDOM = "random"
 const KEYNAME_ALL = "all"
