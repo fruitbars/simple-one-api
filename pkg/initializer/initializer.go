c5c6ae0e72e743b8cd5c045b939001e8b3ca51bc
fruitbars <huiyanghu@gmail.com>
2024-07-12 18:01:06 +0800

    启动部分做成公共函数

--- pkg/initializer/initializer.go
+++ pkg/initializer/initializer.go
@@ -1,1 +1,38 @@
+// pkg/initializer/initializer.go
 package initializer
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"simple-one-api/pkg/config"
+	"simple-one-api/pkg/mylog"
+	"sync"
+)
+
+var once sync.Once
+
+// Setup initializes the configuration and logging system.
+func Setup(configName string) error {
+	var err error
+	once.Do(func() {
+		err = config.InitConfig(configName)
+		if err != nil {
+			log.Println("Error initializing config:", err)
+			return
+		}
+
+		log.Println("config.InitConfig ok")
+
+		if !config.Debug {
+			gin.SetMode(gin.ReleaseMode)
+		}
+
+		mylog.InitLog(config.LogLevel)
+		log.Println("config.LogLevel ok")
+	})
+	return err
+}
+
+func Cleanup() {
+	mylog.Logger.Sync() // Ensure all logs are flushed properly
+}
