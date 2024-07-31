d9657a6ae03121912605c891581850be8c429453
fruitbars <huiyanghu@gmail.com>
2024-07-31 17:58:48 +0800

    翻译接口实现，以及cors策略更新

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"log"
 	"net/http"
 	"simple-one-api/pkg/apis"
@@ -43,33 +44,48 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"}, // 允许所有来源，如果需要限制来源，可以将 "*" 替换为具体的 URL
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Request-Private-Network"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Private-Network"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
 
-	// 设置静态文件夹
-	r.Static("/static", "./static")
-
-	// 设置根路径访问静态文件
-	r.StaticFile("/", "./static/index.html")
-
-	// 动态路由处理所有html文件
-	r.GET("/:filename", func(c *gin.Context) {
-		filename := c.Param("filename")
-		if strings.HasSuffix(filename, ".html") {
-			c.File("./static/" + filename)
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
+	r.OPTIONS("/*path", func(c *gin.Context) {
+		if c.GetHeader("Access-Control-Request-Private-Network") == "true" {
+			c.Header("Access-Control-Allow-Private-Network", "true")
 		}
+		if c.GetHeader("Access-Control-Request-Credentials") == "true" {
+			c.Header("Access-Control-Request-Credentials", "true")
+		}
+		c.Status(204)
 	})
+
+	mylog.Logger.Warn("check EnableWeb config", zap.Bool("config.GSOAConf.EnableWeb", config.GSOAConf.EnableWeb))
+	if config.GSOAConf.EnableWeb {
+		mylog.Logger.Info("web enabled")
+		// 设置静态文件夹
+		r.Static("/static", "./static")
+
+		// 设置根路径访问静态文件
+		r.StaticFile("/", "./static/index.html")
+
+		// 动态路由处理所有html文件
+		r.GET("/:filename", func(c *gin.Context) {
+			filename := c.Param("filename")
+			if strings.HasSuffix(filename, ".html") {
+				c.File("./static/" + filename)
+			} else {
+				c.JSON(http.StatusNotFound, gin.H{"error": "File not found"})
+			}
+		})
+	}
 	// 添加POST请求方法处理
 	//r.POST("/v1/chat/completions", handler.OpenAIHandler)
 	r.GET("/v1/models", apis.ModelsHandler)
 	r.GET("/v1/models/:model", apis.RetrieveModelHandler)
 
-	r.POST("/v2/translate", translation.TranslateHandler)
+	r.POST("/v2/translate", translation.TranslateV2Handler)
+	r.POST("/translate", translation.TranslateV1Handler)
 
 	r.GET("/multimodelcall", mywebui.WSMultiModelCallHandler)
 
@@ -78,8 +94,11 @@ func main() {
 	{
 		// 中间件检查路径是否以 /v1/chat/completions 结尾
 		v1.POST("/*path", func(c *gin.Context) {
-			if strings.HasSuffix(c.Request.URL.Path, "/v1/chat/completions") || strings.HasSuffix(c.Request.URL.Path, "/v1") {
+			if strings.HasSuffix(c.Request.URL.Path, "/v1/chat/completions") || strings.HasSuffix(c.Request.URL.Path, "/chat/completions") || strings.HasSuffix(c.Request.URL.Path, "/v1") {
 				handler.OpenAIHandler(c)
+				return
+			} else if strings.HasSuffix(c.Request.URL.Path, "/v1/translate") {
+				translation.TranslateV1Handler(c)
 				return
 			}
 			c.JSON(http.StatusNotFound, gin.H{"error": "Path not found"})
